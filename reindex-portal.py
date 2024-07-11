4f9d0378691bd67085e226ba2bb2bda35584d413
J-J-E <John.j.Ehlen@gmail.com>
2024-07-11 11:28:43 -0700

    Update reindex-portal.py
    moved credentials to env

--- reindex-portal.py
+++ reindex-portal.py
@@ -1,9 +1,5 @@
 import utils
-
-# Configuration
-portal_url = 'https://domain.com/arcgis'
-admin_username = ''
-admin_password = ''
+from env import portal_url, admin_username, admin_password
 
 token = utils.get_token(portal_url, admin_username, admin_password)
 reindex_response = utils.reindex_portal(portal_url, token)
