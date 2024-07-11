2de38c1f0f265ec7620283d1b3f57c82f63ab575
J-J-E <John.j.Ehlen@gmail.com>
2024-07-11 10:47:00 -0700

    Update example_env.py
    added config for update urls script

--- example_env.py
+++ example_env.py
@@ -1,4 +1,10 @@
 # Configuration
-portal_url = 'https://domaincom/arcgis'
+portal_url = 'https://domain.com/arcgis'
 admin_username = 'Admin_Username'
 admin_password = 'Admin_Password'
+
+
+#Update URLs Configuration
+old_url = 'https://old-domain.com'
+new_url = 'https://new-domain.com'
+owners = [] #if list is left empty, all users from portal will be retrieved and iterated through
