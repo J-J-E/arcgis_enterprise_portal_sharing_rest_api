a88a838648bc4f7f31530942519aa52966e12a9a
J-J-E <John.j.Ehlen@gmail.com>
2024-07-11 10:38:44 -0700

    Update update_portal_item_url.py
    added ability to update all items for all portal users using new function in utils

--- update_portal_item_url.py
+++ update_portal_item_url.py
@@ -4,22 +4,26 @@ import utils
 portal_url = 'https://domain.com/arcgis'
 admin_username = ''
 admin_password = ''
-old_url = 'https://10.168.0.23'
-new_url = 'https://gis-test.filmla.com'
-owners = ['Publisher', 'PortalAdmin', 'Esri', 'System']
+old_url = 'https://old-domain.com'
+new_url = 'https://new-domain.com'
+owners = [] #if list is left empty, all users from portal will be retrieved and iterated through
 
 if __name__ == "__main__":
     token = utils.get_token(portal_url, admin_username, admin_password)
     all_items = []
-    
+
+    if not owners:
+        all_users = utils.query_all_users(portal_url, token)
+        owners = sorted([user['username'] for user in all_users])
+
     for owner in owners:
         print(f"getting content for user {owner}.")
         current_items = utils.get_user_items(portal_url, token, owner)
         print(f" - total items for user {owner}: {len(current_items)}")
         all_items.extend(current_items)
-        
+
     print("\nUpdating Item URLs\n")
-    
+
     for item in all_items:
         item_id = item['id']
         item_title = item['title']
