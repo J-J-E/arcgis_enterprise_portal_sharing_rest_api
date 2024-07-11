836110c004deb19f079ca63408d1e799f53847b4
J-J-E <John.j.Ehlen@gmail.com>
2024-07-11 11:14:42 -0700

    Update register-service.py
    removed old components, getting access credentials from env file now

--- register-service.py
+++ register-service.py
@@ -1,8 +1,6 @@
-# File: register-service.py
 import utils
 import json
-import getpass
-
+from env import portal_url, RS_username, RS_password
 
 def read_input() -> dict:
     input_file_path = "./register-service-input.json"
@@ -15,45 +13,19 @@ def read_input() -> dict:
     except json.JSONDecodeError:
         raise ValueError(f"Error decoding JSON from the file {input_file_path}.")
 
-
-def get_user_credentials():
-    try:
-        username = input("Enter your username: ")
-        if not username:
-            raise ValueError("Username cannot be empty.")
-
-        password = getpass.getpass("Enter your password: ")
-        if not password:
-            raise ValueError("Password cannot be empty.")
-
-        return username, password
-    except Exception as e:
-        raise e
-
-
 def main():
     input_data = read_input()
 
-    if "username" in input_data and "password" in input_data:
-        username = input_data["username"]
-        password = input_data["password"]
-
-        if username == "" or password == "":
-            username, password = get_user_credentials()
-
-    else:
-        username, password = get_user_credentials()
-
+    username = RS_username
+    password = RS_password
     serviceUser = input_data.get("serviceUsername", "")
     servicePass = input_data.get("servicePassword", "")
-
-    portal_root_token = input_data["portalRoot"]
-    portal_root_service = input_data["portalRoot"] + "/sharing/rest"
-
+    portal_root_token = portal_url
+    portal_root_service = portal_url + "/sharing/rest"
     token = utils.get_token(portal_root_token, username, password)
-
     add_item_path = f"/content/users/{username}/addItem"
     add_item_url = portal_root_service + add_item_path
+
 
     for service in input_data["services"]:
         url = f"{service['url']}?f=json&token={token}"
